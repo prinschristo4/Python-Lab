e056ebbac97f87135b97874edb2746575e3a65b1
Prince Christopher <35593592+prinschristo4@users.noreply.github.com>
2019-09-24 11:56:07 +0530

    Add files via upload

---
+++ op_l2.py
@@ -0,0 +1,26 @@
+Python 3.7.3 (default, Aug 20 2019, 17:04:43) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> f =open("test.txt",'w')
+>>> 
+======================= RESTART: /home/it2016b208/1.py =======================
+Number of words in the file : Counter()
+>>> 
+======================= RESTART: /home/it2016b208/1.py =======================
+Number of words in the file : Counter({'Python': 1})
+>>> 
+======================= RESTART: /home/it2016b208/1.py =======================
+Number of words in the file : Counter({'Python': 1, 'is': 1, 'a': 1, 'very': 1, 'good': 1, 'programming': 1, 'language.': 1})
+>>> 
+======================= RESTART: /home/it2016b208/1.py =======================
+Number of words in the file : Counter({'Python': 1, 'is': 1, 'a': 1, 'very': 1, 'good': 1, 'programming': 1, 'language.': 1})
+>>> 
+======================= RESTART: /home/it2016b208/1.py =======================
+Number of words in the file : Counter({'Python': 1, 'is': 1, 'a': 1, 'very': 1, 'good': 1, 'programming': 1, 'language.': 1})
+>>> 
+======================= RESTART: /home/it2016b208/1.py =======================
+Number of words in the file : Counter({'is': 2, 'Python': 1, 'a': 1, 'very': 1, 'good': 1, 'programming': 1, 'language.': 1, 'My': 1, 'name': 1, 'Prince': 1, 'Christopher.': 1})
+>>> 
+======================= RESTART: /home/it2016b208/1.py =======================
+Number of words in the file : Counter({'Python': 3, 'is': 2, 'Prince': 2, 'a': 1, 'very': 1, 'good': 1, 'programming': 1, 'language': 1, 'language.': 1, 'My': 1, 'name': 1, 'Christopher.': 1})
+>>> 
